ed86f45970820784c9ef570062ebd25c58d89c6e
vitorararuna <contato.vitorararuna@gmail.com>
2024-11-11 20:22:06 -0300

    first commit

---
+++ application1/src/main/java/tcc/application1/CustomController.java
@@ -0,0 +1,34 @@
+package tcc.application1;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator")
+public class CustomController {
+
+  private final MeterRegistry meterRegistry;
+
+  public CustomController(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+  }
+
+  @GetMapping("/custom-healthcheck")
+  public String healthCheck() {
+    return "A aplicação está rodando!";
+  }
+
+  @GetMapping("/custom-metrics")
+  public String getMetrics() {
+    // Incrementando um contador para monitoramento
+    meterRegistry.counter("custom.metrics.accessed").increment();
+    return "Métricas acessadas!";
+  }
+
+  @GetMapping("/custom-info")
+  public String getInfo() {
+    return "Informações sobre a aplicação: Esta é uma aplicação monitorada!";
+  }
+}
---
+++ spring-admin/src/test/java/com/tcc/spring_admin/SpringAdminApplicationTests.java
@@ -0,0 +1,9 @@
+package com.tcc.spring_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+}
